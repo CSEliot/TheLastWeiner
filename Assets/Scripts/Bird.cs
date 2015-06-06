445a71a00bc54290ec223b9c4dd5a0ad9560a11a
Alex Grube <alexgrube@Hades.local>
2015-06-06 04:27:12 -0400

    Got bird movement in, plus some collision logic.

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+	public float rotateTime;
+
+	private enum ActionState
+	{
+		IDLE,
+		CARRYING_DOG,
+		CARRYING_COIN,
+		PERFORMING
+	}
+
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+	private ActionState _state;
+
+	private Quaternion _lastRotation;
+	private Quaternion _targetRotation;
+	private float _rotateDx;
+
+
+	// Use this for initialization
+	void Start () {
+		_transform = transform;
+		_rigidbody = GetComponent<Rigidbody2D> ();
+
+		_state = ActionState.IDLE;
+
+		_lastRotation = _transform.rotation;
+		_targetRotation = _transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_state == ActionState.PERFORMING) {
+			_state = ActionState.IDLE;
+		}
+
+		_rotateDx = Mathf.Min (_rotateDx + Time.deltaTime, rotateTime);
+
+		_transform.rotation = Quaternion.Lerp (_lastRotation, _targetRotation, _rotateDx / rotateTime);
+	}
+
+	public void Move(Vector3 direction) {
+		float upangle = -(90f - (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
+
+		_lastRotation = _transform.rotation;
+		_targetRotation = Quaternion.AngleAxis(upangle, Vector3.forward);
+		_rotateDx = 0;
+
+		_rigidbody.velocity = direction;
+	}
+	
+	public void PerformAction() {
+		if (_state == ActionState.CARRYING_COIN) {
+			_state = ActionState.PERFORMING;
+			// grab the coin from the pool and send the coin downwards
+			//_coin.GetComponent<Rigidbody2D>().AddForce();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag.Equals ("Hotdog")) {
+			_state = ActionState.CARRYING_DOG;
+			collider.gameObject.SetActive (false);
+		} else if (collider.gameObject.tag.Equals ("Customer") && _state == ActionState.CARRYING_DOG) {
+			_state = ActionState.CARRYING_COIN;
+		}
+	}
+}
---
+++ Assets/Scripts/BirdControls.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdControls : MonoBehaviour {
+
+	public bool keyboard = true;
+
+	private const string HORIZONTAL_NAME = "BirdHorizontal";
+	private const string VERTICAL_NAME = "BirdVertical";
+	private const string HORIZONTAL_BUTTON_NAME = "BirdHorizontalButton";
+	private const string VERTICAL_BUTTON_NAME = "BirdVerticalButton";
+	private const string ACTION_NAME = "BirdAction";
+	
+	public float speed;
+
+	private Vector3 _direction;
+	private Bird _bird;
+
+	// Use this for initialization
+	void Awake () {
+		this._direction = new Vector3 ();
+		this._bird = this.gameObject.GetComponent<Bird>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!keyboard) {
+			_direction.x = Input.GetAxis (HORIZONTAL_NAME);
+			_direction.y = Input.GetAxis (VERTICAL_NAME);
+		} else {
+			_direction.x = Input.GetAxisRaw (HORIZONTAL_BUTTON_NAME);
+			_direction.y = Input.GetAxisRaw (VERTICAL_BUTTON_NAME);	
+		}
+
+		_direction.z = 0.0f;
+		_direction.Normalize ();
+
+		print (_direction.ToString());
+
+		_bird.Move (_direction * speed * Time.deltaTime);
+
+		if (Input.GetAxis (ACTION_NAME) > 0) {
+			_bird.PerformAction();
+		}
+	}
+}
