6e425123e8283712e356ada7ef14936c59d7857f
Alex Grube <alexgrube@Hades.local>
2015-06-05 23:56:28 -0400

    Set up input manager for movement.

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class BirdController : MonoBehaviour {
 
+	private Transform _transform;
+
 	// Use this for initialization
 	void Start () {
-	
+		this._transform = this.transform;
 	}
 	
 	// Update is called once per frame
