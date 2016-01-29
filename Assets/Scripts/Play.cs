c71ec888b12ef52797f8acfd4458e3fb2fb59f4a
Eliot <eliot2@umbc.edu>
2016-01-29 09:52:58 -0500

    Backing up

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PLAYGAME()
+    {
+        Application.LoadLevel(1);
+    }
+}
